b00ea25449391c9744e827b868b5bd63bc110302
Manfred Lotz <manfred.lotz@posteo.de>
2026-06-14 10:00:22 +0200

    add ghostty installation from terra

--- build/10-build.sh
+++ build/10-build.sh
@@ -64,6 +64,9 @@ dnf5 install -y spice-gtk-tools
 dnf5 install -y \
     kitty
 
+dnf5 install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+dnf5 install -y ghostty ghostty-bat-syntax ghostty-nautilus ghostty-neovim
+
 # Example using COPR with isolated pattern:
 # copr_install_isolated "ublue-os/staging" package-name
 
